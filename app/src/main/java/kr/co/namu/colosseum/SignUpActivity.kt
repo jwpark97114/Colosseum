423a0a5a662d4cd78a90b7a2f5a9bfb0069b7d19
jwpark97114 <jwpark97114@gmail.com>
2020-07-10 05:14:33 -0700

    회원가입 성공, 실패 여부 처리 실패시 사유를 서버에서 받아와 출력

--- app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
@@ -2,6 +2,10 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.namu.colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +15,38 @@ class SignUpActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
+
+//        회원가입 버튼 처리
+        okBtn.setOnClickListener {
+
+//            입력 항목들을 받아오기
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputNick = nickNameEdt.text.toString()
+
+//            입력 항목 값들을 서버에 전달해 회원가입
+            ServerUtil.putRequestSignUp(mContext,inputId,inputPw,inputNick,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+
+                    runOnUiThread {
+                        val code = json.getInt("code")
+
+                        if(code == 200){
+                            Toast.makeText(mContext, "회원가입 성공",Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                        else{
+//                          가입 실패 이유는 다양(이메일, 닉네임 중복) => 실패 이유
+//                          서버가 알려주는 실패 이유를 토스트로 띄우기
+                            val failReason = json.getString("message")
+                            Toast.makeText(mContext,failReason, Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                }
+            })
+        }
 
     }
 
--- app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
@@ -10,17 +10,23 @@ class ServerUtil {
 
 //    액티비티에서 서버의 응답을 받았을 때 처리할 코드를 담아두는 도구
 
-    interface JsonResponseHandler{
-        fun onResponse(json : JSONObject)
+    interface JsonResponseHandler {
+        fun onResponse(json: JSONObject)
 
     }
-    companion object{
-//        서버 접근 주소 담는 변수
+
+    companion object {
+        //        서버 접근 주소 담는 변수
         private val BASE_URL = "http://15.165.177.142"
 
         //            로그인 요청을 해주는 함수 => 화면에서 입력한 아이디 비번 받아야함
 
-        fun postRequestLogin(context: Context,id:String,pw:String, handler:JsonResponseHandler?){
+        fun postRequestLogin(
+            context: Context,
+            id: String,
+            pw: String,
+            handler: JsonResponseHandler?
+        ) {
 
 //            서버 통신 담당 변수
             val client = OkHttpClient()
@@ -32,7 +38,7 @@ class ServerUtil {
 
             val formData = FormBody.Builder()
                 .add("email", id)
-                .add("password",pw)
+                .add("password", pw)
                 .build()
 
 //            요청 정보를 종합하는 변수
@@ -42,7 +48,7 @@ class ServerUtil {
                 .build()
 
 //            실제로 api 호출
-            client.newCall(request).enqueue(object : Callback{
+            client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
 
                 }
@@ -51,7 +57,7 @@ class ServerUtil {
 
 //                    서버 응답이 돌아왔을때 실행되는 영역
 //                    응답 내용을 저장
-                    val bodyStr =response.body?.string()
+                    val bodyStr = response.body?.string()
 
 //                    이 내용을 기반으로 Json 객체 생성
                     val json = JSONObject(bodyStr)
@@ -64,5 +70,61 @@ class ServerUtil {
 
 
         }
+        //            회원가 요청을 해주는 함수 => 아이디, 비번, 닉네임 받아야함
+
+        fun putRequestSignUp(
+            context: Context,
+            id: String,
+            pw: String,
+            nick: String,
+            handler: JsonResponseHandler?
+        ) {
+
+//            서버 통신 담당 변수
+            val client = OkHttpClient()
+
+//            어느 주소로 통신할지
+            val urlStr = "${BASE_URL}/user"
+
+//            서버에 들고갈 짐을 FormData 에 담자 (POST / PUT / PATCH에서 이 방식)
+
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .add("nick_name", nick)
+                .build()
+
+//            요청 정보를 종합하는 변수
+            val request = Request.Builder()
+                .url(urlStr)
+                .put(formData)
+                .build()
+
+//            실제로 api 호출
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    서버 응답이 돌아왔을때 실행되는 영역
+//                    응답 내용을 저장
+                    val bodyStr = response.body?.string()
+
+//                    이 내용을 기반으로 Json 객체 생성
+                    val json = JSONObject(bodyStr)
+                    Log.d("서버 응답 내용", json.toString())
+
+//                    handler 변수에 응답처리 코드가 들어있다면 실행시킬 것
+                    handler?.onResponse(json)
+                }
+            })
+
+
+        }
+
     }
+
 }
+
