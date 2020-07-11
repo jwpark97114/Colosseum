c9b37dec7393c072123fabbf668817bb2423bdd3
jwpark97114 <jwpark97114@gmail.com>
2020-07-11 03:17:41 -0700

    배경 이미지 반영

--- app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.namu.colosseum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,6 +24,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        topicListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val myIntent = Intent(mContext, ViewTopicDetailActivity::class.java)
+
+//            어떤 주제를 눌렀는지 전달
+
+            val clickedTopic = mTopicList[position]
+
+            myIntent.putExtra("topicId", clickedTopic.id)
+
+            startActivity(myIntent)
+
+        }
 
     }
 
--- app/src/main/java/kr/co/namu/colosseum/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/ViewTopicDetailActivity.kt
@@ -2,8 +2,22 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_topic_detail.*
+import kr.co.namu.colosseum.datas.Topic
+import kr.co.namu.colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewTopicDetailActivity : BaseActivity() {
+
+//    메인화면에서 전달해준 클릭된 토픽의 id값
+    var mTopicId = 0
+
+//    서버에서 받아온 토론 topic 상세 정보를 담을 변수
+    lateinit var mTopicData : Topic
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_topic_detail)
@@ -15,6 +29,43 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        메인에서 보내준 토픽 id값을 저장
+
+        mTopicId = intent.getIntExtra("topicId",0)
+
+        getTopicDetailFromServer()
+
+//        서버에서 해당 토픽의 진행상황 상세 정보 확인
+
+
+    }
+
+    fun getTopicDetailFromServer(){
+
+        ServerUtil.getRequestTopicDetail(mContext, mTopicId, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+
+                val topicJson = data.getJSONObject("topic")
+
+//                멤버변수 mTopicData에 서버에서 받아온 내용을 저장
+
+                mTopicData = Topic.getTopicFromJson(topicJson)
+
+                runOnUiThread {
+
+                    titleTxt.text = mTopicData.title
+
+                    Glide.with(mContext).load(mTopicData.imageUrl).into(topicImg)
+
+                }
+
+
+            }
+        })
+
     }
 
 
